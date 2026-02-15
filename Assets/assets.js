7a74d440a82e4c454abb78493cf5ecaf0ea8d7f1
John Izegaegbe <johnstarball@gmail.com>
2026-02-16 00:13:16 +0100

    initiallizing blog page

---
+++ Assets/assets.js
@@ -0,0 +1,122 @@
+import bloglogo1 from './bloglogo1.png';
+import profileicon from './profileicon.png';
+import add2 from './add2.png';
+import ai1 from './ai1.jpg';
+import ai2 from './ai2.jpg';
+import cancel from './cancel.png';
+import facebook from './facebook.png';
+import google from './google.png';
+import image1 from './image1.png';
+import image2 from './image.jpg';
+import image6 from './image6.jpg';
+import image12 from './image12.jpg';
+import wizkid from './wizkid.jpg';
+import x from './x.png';
+import whatsapp from './whatsapp.png';
+import virtual5 from './virtual5.jpg';
+import surf2 from './surf2.jpg';
+import upload1 from './upload1.png';
+import upload2 from './upload2.png';
+import youtube from './youtube.png';
+import instagram from './instagram.png';
+import arrow from './arrow.png';
+import blogicon from './blog-icon.png';s
+import dolphine3 from './dolphine3.jpg';
+import izzywhite from './izzywhite.jpg';
+import trisia1 from './trisia1.jpg';
+import strawberry from './strawberry.jpg';
+import club2 from './club2';
+import dodgered from './dodgered.jpg';
+import image8 from './image8.jpg';
+import image4 from './image4.jpg';
+import email from './email.png';
+
+export const assets = {
+    facebook,
+    google,
+    x,
+    email,
+    upload1,
+    cancel,
+    add1,
+    add2,
+    profileicon,
+    bloglogo1,
+    arrow,
+    blogicon,
+    instagram
+}
+
+
+
+
+export const Blog_data = [{
+    id:1,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:image12,
+    date:Date.now(),
+    category:"Lifestyle",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},
+{   
+    id:2,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:ai1,
+    date:Date.now(),
+    category:"Technology",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},
+{   
+    id:3,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:izzywhite,
+    date:Date.now(),
+    category:"Business",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},
+{   
+    id:4,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:wizkid,
+    date:Date.now(),
+    category:"Education",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},
+{   
+    id:5,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:ai2,
+    date:Date.now(),
+    category:"Health",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},
+{   
+    id:6,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:dolphine3,
+    date:Date.now(),
+    category:"Entertainment",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},
+{   
+    id:7,
+    title: "A detailed step by step guide to manage your lifestyle",
+    description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur quos porro omnis magni dolores officia eius autem commodi quis deleniti minus hic, sunt, quaerat velit totam saepe ut et minima!",
+    image:club2,
+    date:Date.now(),
+    category:"Travel",
+    author:"Pat Chisom",
+    author_img:profile_icon
+},]
---
+++ Components/BlogItem.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BlogItem = () => {
+    return (
+        <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+          
+        </div>
+    )
+}
+
+export default BlogItem
