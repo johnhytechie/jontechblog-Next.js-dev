d5724543d5de1901045c581bf73eae7807dad8ca
John Izegaegbe <johnstarball@gmail.com>
2026-02-17 08:57:28 +0100

    initiate bloglist blogitems pages headers

--- Components/BlogItem.tsx
+++ Components/BlogItem.tsx
@@ -1,23 +1,45 @@
-import { assets, blog_data } from '@/Assets/assets'
+import { assets } from '@/Assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const BlogItem = ({ title, description, category, image }) => {
+type BlogItemProps = {
+  title: string
+  description: string
+  category: string
+  image: any 
+}
 
-    return (
-        <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-red-500 hover:shadow-[-7px_7px_0px_#880000]'>
-          <Image src={image} alt='' width={400} height={400} className='border-b border-red-500'/>
-          <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>{category}</p>
-          <div className='p-5'>
-             <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>{title}</h5>
-             <p className='mb-3 text-sm tracking-tight text-gray-700'>{description}</p>
-             <div className='inline-flex items-center py-2 font-semibold text-center'>
-                Read more <Image src={assets.arrow} className='ml-2' alt='' width={12} height={12}/>
+const BlogItem = ({ title, description, category, image }: BlogItemProps) => {
+  return (
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-red-500 hover:shadow-[-7px_7px_0px_#880000]'>
+      <Image 
+        src={image} 
+        alt='' 
+        width={400} 
+        height={400} 
+        className='border-b border-red-500'
+      />
 
-             </div>
-          </div>
+      <p className='ml-5 mt-5 px-1 inline-block bg-black text-white text-sm'>
+        {category}
+      </p>
+
+      <div className='p-5'>
+        <h5 className='mb-2 text-lg font-medium tracking-tight text-gray-900'>
+          {title}
+        </h5>
+
+        <p className='mb-3 text-sm tracking-tight text-gray-700'>
+          {description}
+        </p>
+
+        <div className='inline-flex items-center py-2 font-semibold'>
+          Read more 
+          <Image src={assets.arrow} className='ml-2' alt='' width={12} height={12}/>
         </div>
-    )
+      </div>
+    </div>
+  )
 }
 
 export default BlogItem
