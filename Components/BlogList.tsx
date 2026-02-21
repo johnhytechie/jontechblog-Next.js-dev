0f02596c50189b7162678914b03fab91b5203825
John Izegaegbe <johnstarball@gmail.com>
2026-02-21 06:52:37 +0100

    initiate jontechblog app setup and page linking

--- Components/BlogList.tsx
+++ Components/BlogList.tsx
@@ -13,15 +13,16 @@ import { blog_data } from '@/Assets/assets'
 
         <button onClick={()=>setMenu('All')} className={menu==="All"? 'bg-black text-white py-1 px-4 rounded-sm':""}>All</button>
         <button onClick={()=>setMenu('Technology')} className={menu==="Technology"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Technology</button>
-        <button onClick={()=>setMenu('Startup')} className={menu==="Startup"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Startup</button>
+        <button onClick={()=>setMenu('Entertainment')} className={menu==="Entertainment"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Entertainment</button>
         <button onClick={()=>setMenu('Lifestyle')} className={menu==="Lifestyle"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Lifestyle</button>
         <button onClick={()=>setMenu('Business')} className={menu==="Business"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Business</button>
         <button onClick={()=>setMenu('Education')} className={menu==="Education"? 'bg-black text-white py-1 px-4 rounded-sm':""} >Education</button>
         <button onClick={()=>setMenu('Health')} className={menu==="Health"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Health</button>
         <button onClick={()=>setMenu('Sports')} className={menu==="Sports"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Sports</button>
+        <button onClick={()=>setMenu('Travel')} className={menu==="Travel"? 'bg-black text-white py-1 px-4 rounded-sm':""}>Travel</button>
        </div>
        <div className='flex flex-wrap justify-around gap-1 gap-y-10 mg-16 xl:mx-24'>
-          {blog_data.map((item,index)=>{
+          {blog_data.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
                return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
           })}
 
---
+++ Components/Footer.tsx
@@ -0,0 +1,14 @@
+import { assets } from '@/Assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex justify-around flex-col gap-2 sm:gap-o sm:gap-o sm:flex-row bg-black py-5 items-center'>
+      <Image src={assets.bloglogo1} alt='' width={120} />
+      <p className='text-sm text-white'>All right reserved. Copyright @Tekinora</p>
+    </div>
+  )
+}
+
+export default Footer
