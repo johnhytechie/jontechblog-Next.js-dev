eab8ddeaba2d6fdc63e7a7676072219a4641e96f
John Izegaegbe <johnstarball@gmail.com>
2026-02-23 20:20:49 +0100

    blog pages setting up

--- app/blogs/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type Props = { params: { id: string } };
-
-const Page = ({ params }: Props) => {
-  return <div>Blog ID: {params.id}</div>;
-};
-
-export default Page;
