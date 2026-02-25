c780eade405f0abe095fe1f6f79bdb57924ff8a4
John Izegaegbe <johnstarball@gmail.com>
2026-02-25 06:01:11 +0100

    blog pages creation and linking

--- Components/Header.tsx
+++ Components/Header.tsx
@@ -1,27 +1,58 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import { assets } from '@/Assets/assets'
 
 const Header = () => {
   return (
     <div className="py-5 px-5 md:px-12 lg:px-28">
+      
       <div className="flex justify-between items-center">
-        <Image 
-          src={assets.tekinoralogo}
-          alt=''
-          width={180}
-          className='w-[130px] sm:w-auto'
-        />
-         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-red shadow-[-7px_7px_0px_#880000]'>Get Started<Image src={assets.arrow} width={20} alt=''/></button>
+
+        {/* Logo Link */}
+        <Link href="/">
+          <Image 
+            src={assets.tekinoralogo}
+            alt="Tekinora Logo"
+            width={180}
+            className='w-[130px] sm:w-auto cursor-pointer'
+          />
+        </Link>
+
+        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-red shadow-[-7px_7px_0px_#880000]'>
+          Get Started
+          <Image src={assets.arrow} width={20} alt=''/>
+        </button>
+
       </div>
+
       <div className='text-center my-8'>
         <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
-        <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sunt eveniet amet consequuntur! A expedita quo veniam neque eaque et, sequi quaerat fugiat natus esse exercitationem quibusdam quod? Nemo, repellat.</p>
+
+        <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia sunt eveniet amet consequuntur! A expedita quo veniam neque eaque et, sequi quaerat fugiat natus esse exercitationem quibusdam quod? Nemo, repellat.
+        </p>
+
         <form className='flex justify-between max-w-[500px] scale-75 sm:scale-100 mx-auto mt-10 border border-red shadow-[-7px_7px_0px_#880000]'>
-            <input name='email' type="email" placeholder='Enter your email' className='pl-4 outline-none'/>
-            <button type='submit' className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'>Subscribe</button>
+
+            <input
+              name='email'
+              type="email"
+              placeholder='Enter your email'
+              className='pl-4 outline-none'
+            />
+
+            <button
+              type='submit'
+              className='border-l border-black py-4 px-4 sm:px-8 active:bg-gray-600 active:text-white'
+            >
+              Subscribe
+            </button>
+
         </form>
+
       </div>
+
     </div>
   );
 };
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,16 +1,78 @@
 import React from 'react'
+import Header from '@/Components/Header'
+import Footer from '@/Components/Footer'
+import Image from 'next/image'
+import { assets } from '@/Assets/assets'
 
 const ContactPage = () => {
   return (
-    <div className='max-w-4xl mx-auto px-4 py-10'>
-      <h1 className='text-3xl font-bold mb-4'>Contact Us</h1>
+    <div>
 
-      <p className='mb-3'>
-        Have questions or suggestions? Reach out to us.
-      </p>
+      {/* Header */}
+      <Header />
 
-      <p>Email: tekinora@gmail.com</p>
-      <p>Phone: +234 XXX XXX XXXX</p>
+      {/* Contact Content */}
+      <div className='max-w-4xl mx-auto px-4 py-10'>
+
+        <h1 className='text-3xl font-bold mb-6 text-center'>
+          Contact Tekinora
+        </h1>
+
+        <div className='flex flex-col sm:flex-row gap-8 items-center'>
+
+          {/* Contact Text */}
+          <div>
+
+            <p className='mb-4 text-gray-700'>
+              Have questions, suggestions, or business inquiries?
+              We'd love to hear from you.
+            </p>
+
+            <p className='mb-6 text-gray-700'>
+              You can reach Tekinora through the contact form below
+              or send us an email anytime.
+            </p>
+
+          </div>
+
+        </div>
+
+        {/* Contact Form */}
+
+        <form className='max-w-[500px] mx-auto flex flex-col gap-4'>
+
+          <input
+            type="text"
+            placeholder="Your Name"
+            className='border p-3 outline-none'
+          />
+
+          <input
+            type="email"
+            placeholder="Your Email"
+            className='border p-3 outline-none'
+          />
+
+          <textarea
+            rows={5}
+            placeholder="Your Message"
+            className='border p-3 outline-none'
+          />
+
+          <button
+            type="submit"
+            className='border py-3 shadow-[-7px_7px_0px_#880000] hover:bg-black hover:text-white'
+          >
+            Send Message
+          </button>
+
+        </form>
+
+      </div>
+
+      {/* Footer */}
+      <Footer />
+
     </div>
   )
 }
